dc4530cb8f5ec91c0814f0b74c371d982e2ad2af
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-08 03:26:49 -0500

    add: car interface

---
+++ src/interfaces/car.interface.ts
@@ -0,0 +1,13 @@
+export default interface Car {
+  carBaggage: any[]
+  category: string
+  companyImageUrl: string
+  dailyRate: number
+  dailyRateCurrency: string
+  doors: number
+  imageUrl: string
+  model: string
+  passengers: number
+  totalRate: number
+  totalRateCurrency: string
+}

eece8ea4752b059525bdba7f3cbe36896caebd50
Daniel Eduardo Almagro <danyeduard17@gmail.com>
2021-12-08 16:55:20 -0500

    create: app routes and implement auth token
    add: generic css styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Suspense, useEffect } from 'react'
+import { useAppDispatch, useAppSelector } from 'hooks'
+import { getAuthToken } from 'services/auth'
+import { setAuth } from 'store/slice/auth'
+import SearchResults from 'pages/SearchResults'
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit <code>src/App.tsx</code> and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
-)
+const App = () => {
+  const dispatch = useAppDispatch()
+  const { isAuthenticated } = useAppSelector((state) => state.AuthReducer)
 
+  useEffect(() => {
+    ;(async () => {
+      if (!isAuthenticated) {
+        const token = await getAuthToken()
+        const auth = { isAuthenticated: true, token }
+        dispatch(setAuth(auth))
+      }
+    })()
+    return () => {}
+  }, [isAuthenticated])
+
+  if (!isAuthenticated) return null
+
+  return (
+    <BrowserRouter>
+      <Suspense fallback={<h1>cargando...</h1>}>
+        <Routes>
+          <Route path="*" element={<p>No Found</p>} />
+          <Route path="/" element={<p>Bienvenido</p>} />
+          <Route
+            path="/challenge/:originCityCode/:destinationCityCode/:pickUpDate/:dropOffDate"
+            element={<SearchResults />}
+          />
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  )
+}
 export default App
